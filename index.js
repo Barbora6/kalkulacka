f89b19dd217d5cfa01f1729ba967d7a2531b5079
Barbora6 <145871155+Barbora6@users.noreply.github.com>
2025-03-24 11:15:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// CALCULATOR
+// https://www.youtube.com/watch?v=I5kj-YsmWjM
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
